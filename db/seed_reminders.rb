5c47b17924d65177f9db2a5c63fb5929979f351b
John Goodman <john@poeticsystems.com>
2011-08-19 00:02:59 -0400

    make seedreminders

---
+++ db/seed_reminders.rb
@@ -0,0 +1,32 @@
+# Recurring - 1 month interval
+CustomerServiceReminder.create(:vehicle_id => 2, :service_reminder_id => 2, :interval => 1, :recurring => true, :max_send => 20, :created_at => "2011-07-15 04:00:00")
+CustomerServiceReminder.create(:vehicle_id => 2, :service_reminder_id => 2, :interval => 1, :recurring => true, :max_send => 20, :created_at => "2011-07-16 04:00:00")
+CustomerServiceReminder.create(:vehicle_id => 2, :service_reminder_id => 2, :interval => 1, :recurring => true, :max_send => 20, :created_at => "2011-07-17 04:00:00")
+CustomerServiceReminder.create(:vehicle_id => 2, :service_reminder_id => 2, :interval => 1, :recurring => true, :max_send => 20, :created_at => "2011-07-18 04:00:00")
+CustomerServiceReminder.create(:vehicle_id => 2, :service_reminder_id => 2, :interval => 1, :recurring => true, :max_send => 20, :created_at => "2011-07-19 04:00:00")
+CustomerServiceReminder.create(:vehicle_id => 2, :service_reminder_id => 2, :interval => 1, :recurring => true, :max_send => 20, :created_at => "2011-07-20 04:00:00")
+CustomerServiceReminder.create(:vehicle_id => 2, :service_reminder_id => 2, :interval => 1, :recurring => true, :max_send => 20, :created_at => "2011-07-21 04:00:00")
+CustomerServiceReminder.create(:vehicle_id => 2, :service_reminder_id => 2, :interval => 1, :recurring => true, :max_send => 20, :created_at => "2011-07-22 04:00:00")
+CustomerServiceReminder.create(:vehicle_id => 2, :service_reminder_id => 2, :interval => 1, :recurring => true, :max_send => 20, :created_at => "2011-07-23 04:00:00")
+CustomerServiceReminder.create(:vehicle_id => 2, :service_reminder_id => 2, :interval => 1, :recurring => true, :max_send => 20, :created_at => "2011-07-24 04:00:00")
+CustomerServiceReminder.create(:vehicle_id => 2, :service_reminder_id => 2, :interval => 1, :recurring => true, :max_send => 20, :created_at => "2011-07-25 04:00:00")
+CustomerServiceReminder.create(:vehicle_id => 2, :service_reminder_id => 2, :interval => 1, :recurring => true, :max_send => 20, :created_at => "2011-07-26 04:00:00")
+CustomerServiceReminder.create(:vehicle_id => 2, :service_reminder_id => 2, :interval => 1, :recurring => true, :max_send => 20, :created_at => "2011-07-27 04:00:00")
+CustomerServiceReminder.create(:vehicle_id => 2, :service_reminder_id => 2, :interval => 1, :recurring => true, :max_send => 20, :created_at => "2011-07-28 04:00:00")
+CustomerServiceReminder.create(:vehicle_id => 2, :service_reminder_id => 2, :interval => 1, :recurring => true, :max_send => 20, :created_at => "2011-07-29 04:00:00")
+CustomerServiceReminder.create(:vehicle_id => 2, :service_reminder_id => 2, :interval => 1, :recurring => true, :max_send => 20, :created_at => "2011-07-30 04:00:00")
+
+
+ => #<CustomerServiceReminder id: 23, vehicle_id: nil, service_reminder_id: 15, sent_on: "2011-08-18", interval: nil, recurring: nil, times_sent: 1, max_send: nil, custom_name: "", custom_description: nil, custom_email_subject: "", custom_email_body: "", created_at: "2011-08-16 02:48:14", updated_at: "2011-08-19 03:16:33", person_id: 21, appointment_date: "2011-08-16 23:56:33", appointment_description: nil>
+ruby-1.9.2-p0 >
+
+CustomerServiceReminder.create(:person_id => 21, :vehicle_id => nil, :service_reminder_id => 15, :interval => nil, :recurring => nil, :max_send => 1, :appointment_date => "2011-07-21 04:00:00")
+CustomerServiceReminder.create(:person_id => 21, :vehicle_id => nil, :service_reminder_id => 15, :interval => nil, :recurring => nil, :max_send => 1, :appointment_date => "2011-07-22 04:00:00")
+CustomerServiceReminder.create(:person_id => 21, :vehicle_id => nil, :service_reminder_id => 15, :interval => nil, :recurring => nil, :max_send => 1, :appointment_date => "2011-07-23 04:00:00")
+CustomerServiceReminder.create(:person_id => 21, :vehicle_id => nil, :service_reminder_id => 15, :interval => nil, :recurring => nil, :max_send => 1, :appointment_date => "2011-07-24 04:00:00")
+CustomerServiceReminder.create(:person_id => 21, :vehicle_id => nil, :service_reminder_id => 15, :interval => nil, :recurring => nil, :max_send => 1, :appointment_date => "2011-07-25 04:00:00")
+CustomerServiceReminder.create(:person_id => 21, :vehicle_id => nil, :service_reminder_id => 15, :interval => nil, :recurring => nil, :max_send => 1, :appointment_date => "2011-07-26 04:00:00")
+CustomerServiceReminder.create(:person_id => 21, :vehicle_id => nil, :service_reminder_id => 15, :interval => nil, :recurring => nil, :max_send => 1, :appointment_date => "2011-07-27 04:00:00")
+CustomerServiceReminder.create(:person_id => 21, :vehicle_id => nil, :service_reminder_id => 15, :interval => nil, :recurring => nil, :max_send => 1, :appointment_date => "2011-07-28 04:00:00")
+CustomerServiceReminder.create(:person_id => 21, :vehicle_id => nil, :service_reminder_id => 15, :interval => nil, :recurring => nil, :max_send => 1, :appointment_date => "2011-07-29 04:00:00")
+CustomerServiceReminder.create(:person_id => 21, :vehicle_id => nil, :service_reminder_id => 15, :interval => nil, :recurring => nil, :max_send => 1, :appointment_date => "2011-07-30 04:00:00")
